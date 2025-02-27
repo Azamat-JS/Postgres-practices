20bdb396a883cb728655c3e2f027a645daf7a899
Azamat-JS <azamatielts987@gmail.com>
2025-02-27 19:51:11 +0500

    comleted CRUD with books and authors in Library project

--- Library/schema/schema.ts
+++ Library/schema/schema.ts
@@ -1,76 +1,205 @@
-import Author from '../models/author.model'
-import Book from '../models/book.model'
+import { parseArgs } from "node:util";
+import Author from "../models/author.model";
+import Book from "../models/book.model";
 
 import {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull,
-    GraphQLEnumType,
-    GraphQLFloat
-  } from 'graphql';
-  
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLFloat,
+  GraphQLInt,
+} from "graphql";
 
-  const AuthorType = new GraphQLObjectType({
-    name: 'writer',
-    fields: () => ({
-      id: { type: GraphQLID },
-      name: { type: GraphQLString }
-    })
+// //////----------------Author type
+
+const AuthorResponseType = new GraphQLObjectType({
+  name: "AuthorResponseType",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    message: { type: GraphQLString },
+    statusCode: { type: GraphQLInt },
+  }),
 });
 
+///----------------- Book type
+const BookResponseType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    price: { type: GraphQLFloat },
+    author: {
+        type: AuthorResponseType,
+        async resolve(parent, args) {
+          console.log("Fetching author for book:", parent.authorId);
+          return await Author.findByPk(parent.authorId);
+        },
+      },      
+    message: { type: GraphQLString },
+    statusCode: { type: GraphQLInt },
+  }),
+});
 
-const BookType = new GraphQLObjectType({
-    name: "Book",
-    fields: () =>({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        price: {type: GraphQLFloat},
-        author: {type: AuthorType,
-            resolve(parent, args){
-                return Author.findByPk(parent.authorId)
-            }
-        }
-    })
-})
+/////////------------ Root query
 
 const RootQuery = new GraphQLObjectType({
-    name: "RootQuery",
-    fields: {
-        authors: {
-            type: new GraphQLList(AuthorType),
-            resolve(parent, args){
-                return Author.findAll()
-            },
-        },
-        
-        author: {
-            type: AuthorType,
-            args: {id: {type: GraphQLID}},
-            resolve(parent, args){
-                return Author.findByPk(args.id)
-            },
-        },
+  name: "RootQuery",
+  fields: {
+    authors: {
+      type: new GraphQLList(AuthorResponseType),
+      resolve(parent, args) {
+        return Author.findAll();
+      },
+    },
 
-        books: {
-            type: new GraphQLList(BookType),
-            resolve(parent, args){
-                return Book.findAll()
-            },
-        },
+    author: {
+      type: AuthorResponseType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Author.findByPk(args.id);
+      },
+    },
 
-        book: {
-            type: BookType,
-            args: {id: {type: GraphQLID}},
-            resolve(parent, args){
-                return Book.findByPk(args.id)
-            }
+    books: {
+      type: new GraphQLList(BookResponseType),
+      resolve(parent, args) {
+        return Book.findAll();
+      },
+    },
+
+    book: {
+      type: BookResponseType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Book.findByPk(args.id);
+      },
+    },
+  },
+});
+
+/// -----------------Mutations
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    //- Add author
+    addAuthor: {
+      type: AuthorResponseType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      async resolve(parent, args) {
+        const author = await Author.create({ name: args.name });
+        return {id:author.id, name:author.name, message:'Author added successfully', statusCode: 201};
+      },
+    },
+
+    //- Update Author
+    updateAuthor: {
+      type: AuthorResponseType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      async resolve(parent, args) {
+        const author = await Author.findByPk(args.id);
+        if (!author) {
+          return { message: "Author not found", statusCode: 404 };
         }
+        return author?.update({ name: args.name });
+      },
+    },
+    // - Delete Author
+    deleteAuthor: {
+      type: AuthorResponseType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      async resolve(parent, args) {
+        const author = await Author.findByPk(args.id);
+        if (!author) {
+          return { message: "Author not found", statusCode: 404 };
+        }
+        await Author.destroy({ where: { id: args.id } });
+        return {id:args.id, message: "Author deleted successfully", statusCode: 200 };
+      },
+    },
+
+    // -- addBook
+    addBook: {
+      type: BookResponseType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        price: { type: GraphQLNonNull(GraphQLFloat) },
+        authorId: { type: GraphQLNonNull(GraphQLID) },
+      },
+      async resolve(parent, args) {
+try {
+    const author = await Author.findByPk(args.authorId);
+    if (!author) {
+      return { message: "Author not found", statusCode: 404 };
     }
-})
+
+    return Book.create({
+      name: args.name,
+      price: args.price,
+      authorId: args.authorId,
+    });
+} catch (error) {
+    return { message: "Error adding book", statusCode: 500 };
+}
+      },
+    },
+
+    //- delete book
+
+    deleteBook:{
+        type: BookResponseType,
+        args:{
+            id: {type: GraphQLNonNull(GraphQLID)}
+        },
+        async resolve(parent, args){
+            const book = await Book.findByPk(args.id)
+            if(!book){
+                return {message: 'Book not found', statusCode: 404}
+            }
+            await Book.destroy({where: {id: args.id}})
+            return {message: 'Book deleted successfully', statusCode: 200}
+        }
+    },
+
+    //- update book
+   updateBook: {
+    type: BookResponseType,
+    args:{
+        id: {type: GraphQLNonNull(GraphQLID)},
+        name: {type: GraphQLString},
+        price: {type: GraphQLFloat},
+        authorId: {type: GraphQLID}
+    },
+   async resolve(parent, args){
+        const book = await Book.findByPk(args.id)
+        if(!book){
+            return {message: 'Book not found', statusCode: 404}
+        }
+       return book?.update({
+            name: args.name,
+            price: args.price,
+            authorId: args.authorId
+        })
+
+    }
+   }
+    
+  },
+});
 
 export default new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+  mutation,
+});
